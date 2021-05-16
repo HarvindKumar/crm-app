6765bdc3935a8ff1ca1aed671696faffaa977fc5
HarvindKumar <83906814+HarvindKumar@users.noreply.github.com>
2021-05-16 12:55:30 +0000

    Feature/contact (#1)
    * Started the development of Contact feature in CRM app.
    
    * Contact feature development is in progress.
    
    * Finished with Contact feature development.

---
+++ core/contact.py
@@ -0,0 +1,18 @@
+# This Python module contains all functionalities of
+# Contact feature of this CRM app.
+
+# Create Contact.
+def create_contact():
+  print "Code logic to create new contact."
+
+
+
+
+ # Update Contact.
+def update_contact():
+  print "Code logic to update contact."
+
+
+  # Delete Contact.
+def delete_contact():
+  print "Code logic to delete contact."
